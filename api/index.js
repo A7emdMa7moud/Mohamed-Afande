348273e90f035888502f39c852b1de72a2885670
Ahmed Mahmoud <153916812+A7emdMa7moud@users.noreply.github.com>
2026-03-11 03:52:01 +0200

    Remove vercel.json and api/index.js files as part of project cleanup

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('../app');
-const connectDB = require('../config/db');
-
-let connecting = connectDB();
-
-module.exports = async (req, res) => {
-  await connecting;
-  app(req, res);
-};
